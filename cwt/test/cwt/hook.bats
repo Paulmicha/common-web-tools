a494f3123b025cf8b97382b5daf9da43fcdf71ce
Paulmicha <paul.michalet@gmail.com>
2017-12-31 10:17:56 -0200

    [wip] Failed tests backup.

--- cwt/test/cwt/hook.bats
+++ cwt/test/cwt/hook.bats
@@ -1,6 +1,15 @@
 #!/usr/bin/env bats
 
+setup() {
+  . cwt/bootstrap.sh
+}
+
 @test "1. Single action hook" {
-  result="$(bash cwt/custom/debug.sh)"
-  [ -n "$result" ]
+  # result="$(hook -a 'bootstrap')"
+  # [ -n "$result" ]
+  run hook -a 'bootstrap'
+  my_var="$CWT_INC"
+  # result="test"
+  # [ -n "$result" ]
+  [ -n "$my_var" ]
 }
--- cwt/test/run.sh
+++ cwt/test/run.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
 ##
-# [wip] Run tests.
+# TODO [wip] Run tests.
 #
-# Usage :
-# $ cwt/test/run.sh
+# TODO evaluate plan : forward all arguments of this script to a hook call
+# with predefined 'test' subject (that could be prefixed or suffixed with custom
+# values).
+#
+# E.g. :
+# $ hook -s 'test' -a "$1"
+#
+# @example (planned)
+#   . cwt/bootstrap.sh
+#   . cwt/test/run.sh 'cwt'
+#
+# @example (current WIP)
+#   cwt/test/run.sh
 #
 
-# [wip] debug.
-for file in $(find cwt/test/cwt -maxdepth 1 -type f -print0 | xargs -0); do
-  bats "$file"
+. cwt/bootstrap.sh
+
+# TODO prereq BATS installed - to provision (or bundle in a 'vendors' dir ?)
+tf_path=cwt/test/cwt
+bats_files=$(u_fs_file_list $tf_path 1 '*.bats')
+
+for file in $bats_files; do
+  bats "$tf_path/$file"
 done
