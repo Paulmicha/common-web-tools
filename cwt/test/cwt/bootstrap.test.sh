3afe1d0d9edfee1f77e88ba47273b1758ef87cee
Paulmicha <paul.michalet@gmail.com>
2018-03-10 00:58:24 -0300

    Add autoload override test.

--- cwt/test/cwt/bootstrap.test.sh
+++ cwt/test/cwt/bootstrap.test.sh
@@ -26,7 +26,7 @@ test_cwt_has_essential_globals() {
 }
 
 ##
-# Does the 'complement' extension mechanism work ?
+# Does the 'complement' alteration mechanism work ?
 #
 test_cwt_autoload_complement_works() {
   local complement_flag
@@ -35,7 +35,7 @@ test_cwt_autoload_complement_works() {
   # Test without match.
   complement_flag=''
   u_autoload_get_complement "$complement_source"
-  assertTrue 'Flag should be empty at this stage ("complement" autoload extension mechanism failed)' "[ -e $complement_flag ]"
+  assertTrue 'Flag should be empty at this stage ("complement" alteration mechanism failed)' "[ -e $complement_flag ]"
 
   # Test with match (populates the local complement_flag variable).
   local base_dir='cwt/custom'
@@ -48,7 +48,35 @@ test_cwt_autoload_complement_works() {
 complement_flag='not-empty'
 EOF
   u_autoload_get_complement "$complement_source"
-  assertFalse 'Flag should not be empty at this stage ("complement" autoload extension mechanism failed)' "[ -e \"$complement_flag\" ]"
+  assertFalse 'Flag should not be empty at this stage ("complement" alteration mechanism failed)' "[ -e $complement_flag ]"
+}
+
+##
+# Does the 'override' alteration mechanism work ?
+#
+test_cwt_autoload_override_works() {
+  local override_flag
+  local override_source='cwt/test/self.sh'
+
+  # Test without match.
+  override_flag=''
+  u_autoload_override "$override_source" 'override_flag="NOK"'
+  eval "$inc_override_evaled_code"
+  assertTrue 'Flag should be empty at this stage ("override" alteration mechanism failed)' "[ -e $override_flag ]"
+
+  # Test with match (populates the local override_flag variable).
+  local base_dir='cwt/custom'
+  if [[ -n "$CWT_CUSTOM_DIR" ]]; then
+    base_dir="$CWT_CUSTOM_DIR"
+  fi
+  mkdir -p "$base_dir/overrides/test"
+  cat > ${override_source/cwt/"$base_dir/overrides"} <<'EOF'
+#!/usr/bin/env bash
+override_flag='not-empty'
+EOF
+  u_autoload_override "$override_source" '# (we have to pass some inoperant code here to carry on with the test)'
+  eval "$inc_override_evaled_code"
+  assertFalse 'Flag should not be empty at this stage ("override" alteration mechanism failed)' "[ -e $override_flag ]"
 }
 
 ##
@@ -62,6 +90,7 @@ oneTimeTearDown() {
     base_dir="$CWT_CUSTOM_DIR"
   fi
   rm -rf "$base_dir/complements/test"
+  rm -rf "$base_dir/overrides/test"
 }
 
 # Load and run shUnit2.
