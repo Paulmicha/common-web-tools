bf6736336c87191e62b7d87d4eca5fa6dd3095a6
Paulmicha <paul.michalet@gmail.com>
2018-04-25 01:00:53 -0300

    Update cwt/remote/remote.inc.sh.

--- cwt/remote/remote.inc.sh
+++ cwt/remote/remote.inc.sh
@@ -8,6 +8,168 @@
 #
 
 ##
+# Downloads a file or dir to given remote.
+#
+# @example
+#   u_remote_download 'my_short_id' /path/to/ /opt/my.project/file.ext
+#
+u_remote_download() {
+  local p_id="$1"
+  local p_local_path="$2"
+  local p_remote_path="$3"
+  shift 3
+
+  u_remote_instance_load "$p_id"
+
+  if [[ -z "$REMOTE_INSTANCE_CONNECT_CMD" ]]; then
+    echo >&2
+    echo "Error in $BASH_SOURCE line $LINENO: no conf found for remote id '$p_id'." >&2
+    echo "-> Aborting (1)." >&2
+    echo >&2
+    return 1
+  fi
+
+  scp "${REMOTE_USER}@${REMOTE_INSTANCE_HOST}:$p_remote_path" "$p_local_path" $@
+
+  if [[ $? -ne 0 ]]; then
+    echo >&2
+    echo "Error in $BASH_SOURCE line $LINENO: the command 'scp' exited with a non-zero status." >&2
+    echo >&2
+    exit 1
+  else
+    echo "Download successfully completed."
+  fi
+}
+
+##
+# Uploads a file or dir to given remote dir (include last slash) or filepath.
+#
+# @example
+#   u_remote_upload 'my_short_id' /path/to/file.ext /opt/my.project/
+#
+u_remote_upload() {
+  local p_id="$1"
+  local p_local_path="$2"
+  local p_remote_path="$3"
+  shift 3
+
+  u_remote_instance_load "$p_id"
+
+  if [[ -z "$REMOTE_INSTANCE_CONNECT_CMD" ]]; then
+    echo >&2
+    echo "EFFFLINENO: no conf found for remote id '$p_id'." >&2
+    echo "-> Aborting (1)." >&2
+    echo >&2
+    return 1
+  fi
+
+  scp "$p_local_path" "${REMOTE_USER}@${REMOTE_INSTANCE_HOST}:${p_remote_path}" $@
+
+  if [[ $? -ne 0 ]]; then
+    echo >&2
+    echo "Error in $BASH_SOURCE line $LINENO: the command 'scp' exited with a non-zero status." >&2
+    echo >&2
+    exit 1
+  else
+    echo "Upload successfully completed."
+  fi
+}
+
+##
+# Add Local SSH keys to remote host(s) authorized keys.
+#
+# Prerequisites:
+# - Local SSH keys created and loaded in local shell session (ssh-agent).
+#
+# Calling this function will :
+# - Add remote host to local "known_hosts" by connecting once via ssh.
+# - Send public key to remote server user's "authorized_keys" file.
+#
+# Note : these steps will prompt for confirmation and/or passwords. After it's
+# done, ssh should work without these prompts.
+#
+# @see u_remote_script_wrapper()
+#
+# @example
+#   u_remote_authorize_ssh_key 'my_short_id'
+#
+u_remote_authorize_ssh_key() {
+  local p_id="$1"
+  local p_key="$2"
+
+  local public_key_path="$HOME/.ssh/id_rsa.pub"
+  if [[ -n "$p_key" ]]; then
+    public_key_path="$p_key"
+  fi
+
+  u_remote_instance_load "$p_id"
+
+  if [[ -z "$REMOTE_INSTANCE_CONNECT_CMD" ]]; then
+    echo >&2
+    echo "Error in $BASH_SOURCE line $LINENO: no conf found for remote id '$p_id'." >&2
+    echo "-> Aborting (1)." >&2
+    echo >&2
+    return 1
+  fi
+
+  # Current $USER must already have a public key.
+  if [ ! -f "$public_key_path" ]; then
+    echo >&2
+    echo "Error in $BASH_SOURCE line $LINENO: the public key '$public_key_path' was not found." >&2
+    echo "E.g. generate with command : ssh-keygen -t rsa" >&2
+    echo "-> Aborting (2)." >&2
+    echo >&2
+    return 2
+  fi
+
+  # Prevent running this more than once per host.
+  # TODO support other implementations of "once" mecanism (currently file based).
+  if ! $(u_check_once "u_remote_authorize_ssh_key.${REMOTE_INSTANCE_HOST}.$USER.${public_key_path}" 'host'); then
+    echo
+    echo "Notice in $BASH_SOURCE line $LINENO: it appears that key was already sent to that remote host."
+    echo "There is no need to send it again."
+    echo "-> Aborting (0)."
+    echo
+    return 0
+  fi
+
+  # Ensures SSH agent is running with the key loaded.
+  if [ -z "$SSH_AUTH_SOCK" ]; then
+    echo "SSH agent is not running (or not detected in $BASH_SOURCE line $LINENO)"
+    echo "-> Launching ssh-agent and load the key in current terminal session..."
+    echo "Note : if a passphrase was used to generate the key, this will prompt for it."
+
+    eval `ssh-agent -s`
+    ssh-add "$public_key_path"
+
+    if [[ $? -ne 0 ]]; then
+      echo >&2
+      echo "Error in $BASH_SOURCE line $LINENO: the command 'ssh-add' exited with a non-zero status." >&2
+      echo "-> Aborting (4)." >&2
+      echo >&2
+      exit 4
+    else
+      echo "Launching ssh-agent and load the key in current terminal session : done."
+    fi
+  fi
+
+  echo
+  echo "Sending our local key to the remote server 'authorized_keys' file..."
+  echo "Note : this may prompt for confirmation for adding the remote host to the local 'known_hosts' file if it's the first time a connexion is made."
+  echo
+
+  eval "cat $public_key_path | $REMOTE_INSTANCE_CONNECT_CMD 'cat >> .ssh/authorized_keys'"
+
+  echo "Ok, now the following call should not prompt for password, and should print the IP address of the remote host '$REMOTE_INSTANCE_HOST' :"
+  echo
+
+  eval "$REMOTE_INSTANCE_CONNECT_CMD -t ip route get 1"
+
+  echo "Over."
+  echo
+}
+
+##
 # Executes local scripts remotely.
 #
 # @param 1 String : remote instance's id (short name, no space, _a-zA-Z0-9 only).
@@ -31,12 +193,12 @@
 #
 u_remote_script_wrapper() {
   local p_id="$1"
-  local p_local_script_to_exec_remotely="$2"
+  local p_remote_script="$2"
   shift 2
 
-  if [[ ! -f "$p_local_script_to_exec_remotely" ]]; then
+  if [[ ! -f "$p_remote_script" ]]; then
     echo
-    echo "Error in $BASH_SOURCE line $LINENO: file '$p_local_script_to_exec_remotely' not found." >&2
+    echo "Error in $BASH_SOURCE line $LINENO: file '$p_remote_script' not found." >&2
     echo "Aborting (1)." >&2
     echo
     return 1
@@ -58,10 +220,10 @@ u_remote_script_wrapper() {
     local args
     printf -v args '%q ' "$@"
 
-    eval "$REMOTE_INSTANCE_CONNECT_CMD \"cd $REMOTE_INSTANCE_PROJECT_DOCROOT && bash -s -- $args\" < \"$p_local_script_to_exec_remotely\""
+    eval "$REMOTE_INSTANCE_CONNECT_CMD \"cd $REMOTE_INSTANCE_PROJECT_DOCROOT && bash -s -- $args\" < \"$p_remote_script\""
 
   else
-    eval "$REMOTE_INSTANCE_CONNECT_CMD \"cd $REMOTE_INSTANCE_PROJECT_DOCROOT && bash -s\" < \"$p_local_script_to_exec_remotely\""
+    eval "$REMOTE_INSTANCE_CONNECT_CMD \"cd $REMOTE_INSTANCE_PROJECT_DOCROOT && bash -s\" < \"$p_remote_script\""
   fi
 }
 
