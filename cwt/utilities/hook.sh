Paulmicha/common-web-tools

commit f9a4bdebedffa94326c19949fdb9d5651667a3df

[wip] Adapt CWT extensions path to accomodate 'scripts/cwt/extend'.

cwt/utilities/hook.sh
@@ -159,6 +159,7 @@ hook() {
   local base_paths=("cwt")
   local extension
   local uppercase
+  local ext_path
 
   # Allow using only a particular extension (see the '-p' argument).
   if [ -n "$p_extensions_filter" ]; then
@@ -169,7 +170,9 @@ hook() {
       eval "subjects=\"\$${uppercase}_SUBJECTS\""
       eval "actions=\"\$${uppercase}_ACTIONS\""
       # Override base path for lookups.
-      base_paths=("cwt/extensions/$extension")
+      ext_path=''
+      u_cwt_extension_path "$extension"
+      base_paths=("$ext_path/$extension")
     done
 
   # By default, any extension can append its own "primitives".
@@ -184,7 +187,9 @@ hook() {
       eval "subjects+=\" \$${uppercase}_SUBJECTS\""
       eval "actions+=\" \$${uppercase}_ACTIONS\""
       # Every extension defines an additional base path for lookups.
-      base_paths+=("cwt/extensions/$extension")
+      ext_path=''
+      u_cwt_extension_path "$extension"
+      base_paths=("$ext_path/$extension")
     done
   fi
 

cwt/env/global_lookup_paths.make.sh
@@ -23,8 +23,10 @@ hook -a 'global' -c 'vars.sh' -v 'PROVISION_USING' -t -d
 # Allow extra lookup paths at the root of extensions.
 if [ -n "$CWT_EXTENSIONS" ]; then
   for extension in $CWT_EXTENSIONS; do
-    echo "cwt/extensions/$extension/global.vars.sh"
-    if [ -f "cwt/extensions/$extension/global.vars.sh" ]; then
+    ext_path=''
+    u_cwt_extension_path "$extension"
+    echo "$ext_path/$extension/global.vars.sh"
+    if [ -f "$ext_path/$extension/global.vars.sh" ]; then
       echo "  exists"
     fi
   done

cwt/instance/instance.inc.sh
@@ -382,6 +382,7 @@ u_instance_write_mk() {
 
   local task
   local sa_pair
+  local ext_path
 
   # No need to check for collisions in CWT core (we know there aren't any).
   for sa_pair in $CWT_ACTIONS; do
@@ -423,7 +424,9 @@ u_instance_write_mk() {
         fi
 
         mk_tasks+=("$task")
-        mk_entry_points+=("cwt/extensions/$extension/$sa_pair.sh")
+        ext_path=''
+        u_cwt_extension_path "$extension"
+        mk_entry_points+=("$ext_path/$extension/$sa_pair.sh")
       done
 
     fi

cwt/utilities/cwt.sh
@@ -42,11 +42,13 @@
 #   have the same role as the 'subjects' ones described in 1 but must be placed
 #   inside relevant subject's folder.
 #
-# 3. CWT_EXTENSIONS contains a list of folders using the same structure as
-#   the 'cwt' folder. The primitive mecanisms explained in 1 & 2 above apply
-#   to each one of these extensions.
+# 3. CWT_EXTENSIONS contains a list of all active extensions' folder names. Each
+#   one uses the same structure as the 'cwt' folder. The primitive mecanisms
+#   explained in 1 & 2 above apply to each one of these extensions.
 #   Important notes : extensions' folder names can only contain the following
 #   characters : A-Z a-z 0-9 dots . underscores _ dashes -
+#   Exception : the name 'extend' is reserved for project-specific
+#   implementations.
 #
 # 4. The 'CWT_INC' values are a simple list of files to be sourced in
 #   cwt/bootstrap.sh scope directly. They are meant to contain bash functions
@@ -170,16 +172,32 @@ u_cwt_extensions() {
   # project-specific operations (non-reusable).
   custom_extend_path="scripts/cwt/extend"
   if [[ -d "$custom_extend_path" ]]; then
-
-    # TODO [wip] Workaround by using reserved keyword ? -> adapt everywhere ?
     CWT_EXTENSIONS+="extend "
-
     u_cwt_extend "$custom_extend_path"
-    inc="$custom_extend_path/custom.inc.sh"
+    inc="$custom_extend_path/extend.inc.sh"
     if [[ -f "$inc" ]]; then
       CWT_INC+="$inc "
     fi
   fi
+}
+
+##
+# Get extension path by name.
+#
+# @requires local var $ext_path in calling scope.
+# This function modifies an existing variable for performance reasons (in order
+# to avoid using a subshell).
+#
+# @example
+#   ext_path=''
+#   u_cwt_extension_path 'extend'
+#   echo "$ext_path" # Yields 'scripts/cwt'
+#
+u_cwt_extension_path() {
+  ext_path='cwt/extensions'
+  case "$1" in 'extend')
+    ext_path='scripts/cwt'
+  esac
 }
 
 ##
@@ -425,6 +443,7 @@ u_cwt_get_actions() {
   local bp
   local extension
   local uppercase
+  local ext_path
 
   cwt_action_names=()
   cwt_action_scripts=()
@@ -435,7 +454,9 @@ u_cwt_get_actions() {
     u_str_uppercase "$uppercase"
     eval "subjects+=\" \$${uppercase}_SUBJECTS\""
     eval "actions+=\" \$${uppercase}_ACTIONS\""
-    base_paths+=("cwt/extensions/$extension")
+    ext_path=''
+    u_cwt_extension_path "$extension"
+    base_paths+=("$ext_path/$extension")
   done
 
   for s in $subjects; do

cwt/utilities/global.sh
@@ -107,8 +107,10 @@ u_global_lookup_paths() {
   if [ -n "$CWT_EXTENSIONS" ]; then
     local extension
     for extension in $CWT_EXTENSIONS; do
-      if [ -f "cwt/extensions/$extension/global.vars.sh" ]; then
-        global_lookup_paths+="cwt/extensions/$extension/global.vars.sh "
+      ext_path=''
+      u_cwt_extension_path "$extension"
+      if [ -f "$ext_path/$extension/global.vars.sh" ]; then
+        global_lookup_paths+="$ext_path/$extension/global.vars.sh "
       fi
     done
   fi

commit d2e6a11ce9c7386182098190893871e0ec6954e6

Fix regression in hook() introduced in f9a4bde.

cwt/utilities/hook.sh
@@ -189,7 +189,7 @@ hook() {
       # Every extension defines an additional base path for lookups.
       ext_path=''
       u_cwt_extension_path "$extension"
-      base_paths=("$ext_path/$extension")
+      base_paths+=("$ext_path/$extension")
     done
   fi
 