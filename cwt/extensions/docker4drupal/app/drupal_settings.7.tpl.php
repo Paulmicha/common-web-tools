2990df12e4c621ff29c5cfe0705b3613844f00a8
Paulmicha <paul.michalet@gmail.com>
2019-02-16 22:40:19 -0200

    Update Redis settings in default d4d Drupal 7 template.

--- cwt/extensions/docker4drupal/app/drupal_settings.7.tpl.php
+++ cwt/extensions/docker4drupal/app/drupal_settings.7.tpl.php
@@ -10,7 +10,7 @@
  * -> Do not edit directly (will be overwritten).
  *
  * Details in dev stack :
- * @see scripts/cwt/extend/app/drupal_settings.tpl.php
+ * @see cwt/extensions/docker4drupal/app/drupal_settings.7.tpl.php
  * @see cwt/extensions/docker4drupal/docker4drupal.inc.sh
  */
 
@@ -28,5 +28,12 @@ $conf['file_temporary_path'] = '__replace_this_DRUPAL_TMP_DIR_C_value__';
 $conf['file_private_path'] = '__replace_this_DRUPAL_PRIVATE_DIR_C_value__';
 
 $conf['redis_client_interface'] = 'PhpRedis';
+$conf['lock_inc'] = 'sites/all/modules/contrib/redis/redis.lock.inc';
+$conf['path_inc'] = 'sites/all/modules/contrib/redis/redis.path.inc';
+$conf['cache_backends'][] = 'sites/all/modules/contrib/redis/redis.autoload.inc';
+$conf['cache_default_class'] = 'Redis_Cache';
+$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
 $conf['redis_client_host'] = '__replace_this_REDIS_CLIENT_HOST_value__';
 $conf['redis_client_port'] = '__replace_this_REDIS_CLIENT_PORT_value__';
+// $conf['redis_client_base'] = __replace_this_REDIS_CLIENT_BASE_value__;
+// $conf['redis_client_password'] = '__replace_this_REDIS_CLIENT_PASSWORD_value__';
